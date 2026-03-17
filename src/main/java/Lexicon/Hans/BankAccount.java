0cbaef57784932f352cea52ef43ed1d421bcdc79
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-17 13:59:29 +0100

    modify BankAccount.java file
    add get and setters

--- src/main/java/Lexicon/Hans/BankAccount.java
+++ src/main/java/Lexicon/Hans/BankAccount.java
@@ -3,4 +3,20 @@ package Lexicon.Hans;
 public class BankAccount {
     private String accountHolder;
     private int balance;
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
