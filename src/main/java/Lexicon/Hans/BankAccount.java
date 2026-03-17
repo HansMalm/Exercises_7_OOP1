f01a43aa9bd4696ef99a4ad7eee95a090cf9413f
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-03-17 14:21:35 +0100

    modify BankAccount.java file
    add default balance constructor

--- src/main/java/Lexicon/Hans/BankAccount.java
+++ src/main/java/Lexicon/Hans/BankAccount.java
@@ -4,6 +4,15 @@ public class BankAccount {
     private String accountHolder;
     private int balance;
 
+    public BankAccount(String accountHolder, int balance) {
+        setAccountHolder(accountHolder);
+        setBalance(balance);
+    }
+
+    public BankAccount(String accountHolder) {
+        this(accountHolder, 0);
+    }
+
     public String getAccountHolder() {
         return accountHolder;
     }
